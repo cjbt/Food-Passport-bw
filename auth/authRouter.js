7eaedd9a045cc41344aaeb4b04f742796dc31ba3
cecil john tantay <cjbtantay@gmail.com>
2019-05-30 18:54:06 -0700

    adds token in register

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -15,7 +15,16 @@ route.post('/register', (req, res) => {
     const hash = bcrypt.hashSync(password, 10);
     db.register({ username, password: hash, email })
       .then(() => {
-        res.status(201).json({ message: `You have registered, ${username}!` });
+        const token = jwt.sign(user, process.env.JWT_SECRET, {
+          expiresIn: `30 days`
+        });
+        res
+          .status(201)
+          .json({
+            message: `You have registered, ${username}!`,
+            token: token,
+            user: username
+          });
       })
       .catch(err => res.status(500).json(err));
   }
